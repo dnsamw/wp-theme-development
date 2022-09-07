a08d5c764b11407356b4cee5d38a9c51f3638a57
Dileepa Samarawickrama <dnsam.mit@gmail.com>
2022-09-07 22:09:47 +0530

    WordPress post loop

--- index.php
+++ index.php
@@ -9,7 +9,8 @@ if (have_posts()) :
 
     while (have_posts()) :
         the_post();
-    //whtever you want to do with the post
+        //whtever you want to do with the post
+        get_template_part('template-parts/post/content');
     endwhile;
 
 endif;
